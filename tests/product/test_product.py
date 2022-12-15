1d43ea8cab24198b7dd3251bfaa2b5b9ae4416a8
hp <hp@pop-os.localdomain>
2022-12-15 14:59:04 -0300

    feat: requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Alexa",
+        "Amazon",
+        "15/12/2022",
+        "15/12/2023",
+        "123456789",
+        "Armazenar em local seco",
+    )
+    assert produto.id == 1
+    assert produto.nome_do_produto == "Alexa"
+    assert produto.nome_da_empresa == "Amazon"
+    assert produto.data_de_fabricacao == "15/12/2022"
+    assert produto.data_de_validade == "15/12/2023"
+    assert produto.numero_de_serie == "123456789"
+    assert produto.instrucoes_de_armazenamento == "Armazenar em local seco"
+
