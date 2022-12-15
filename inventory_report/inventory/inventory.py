a198c915eb2b99b3ac8166ed1535403d336d81f6
hp <hp@pop-os.localdomain>
2022-12-15 19:05:10 -0300

    feat: requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,19 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
-    @classmethod
-    def import_data(cls, path, type):
+    @staticmethod
+    def import_data(path, type):
+        if "csv" in path:
+            return Inventory.open_csv(path, type)
+        elif "json" in path:
+            return Inventory.open_json(path, type)
+
+    @staticmethod
+    def open_csv(path, type):
         with open(path, encoding="utf-8") as file:
             file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
             if type == "simples":
@@ -15,5 +23,13 @@ class Inventory:
             else:
                 raise ValueError("Tipo inválido")
 
-
-# print(Inventory.import_data("inventory_report/data/inventory.csv", "simples"))
+    @staticmethod
+    def open_json(path, type):
+        with open(path) as file:
+            file_reader = json.load(file)
+            if type == "simples":
+                return SimpleReport.generate(file_reader)
+            elif type == "completo":
+                return CompleteReport.generate(file_reader)
+            else:
+                raise ValueError("Tipo inválido")
