9b950301ce1ced5885b6fde788c84fd6b8b7fab4
hp <hp@pop-os.localdomain>
2022-12-16 11:09:22 -0300

    feat: feito o teste de relatorios produtos

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Alexa",
+        "Amazon",
+        "15/12/2022",
+        "15/12/2023",
+        "123456789",
+        "Armazenar em local seco",
+    )
+    assert ("Alexa" in produto.__repr__()) is True
+    assert ("Amazon" in produto.__repr__()) is True
+    assert ("15/12/2022" in produto.__repr__()) is True
+    assert ("15/12/2023" in produto.__repr__()) is True
+    assert ("Armazenar em local seco" in produto.__repr__()) is True
